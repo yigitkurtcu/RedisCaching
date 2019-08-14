57ccd53642939044d01143666c11f7c1897e12fc
Yiğit Kurtçu <yiqit-35@hotmail.com>
2019-08-14 19:40:36 +0300

    project init

---
+++ routes/blogRoutes.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
+const cleanCache = require('../middlewares/cleanCache');
+const requireLogin = require('../middlewares/requireLogin');
+const Blog = mongoose.model('Blog');
+
+module.exports = app => {
+  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+    const blog = await Blog.findOne({
+      _user: req.user.id,
+      _id: req.params.id
+    });
+
+    res.send(blog);
+  });
+
+  app.get('/api/blogs', requireLogin, async (req, res) => {
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id
+    });
+
+    res.send(blogs);
+  });
+
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
+    const { title, content } = req.body;
+
+    const blog = new Blog({
+      title,
+      content,
+      _user: req.user.id
+    });
+
+    try {
+      await blog.save();
+      res.send(blog);
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  });
+};
